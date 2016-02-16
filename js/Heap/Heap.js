59d3eb4c3acf24e1785c0b86d691eaf6c531757d
Alex Szabo <delanni.alex@gmail.com>
2016-02-16 17:50:07 +0100

    Heap

--- js/Heap/Heap.js
+++ js/Heap/Heap.js
@@ -1,26 +1,142 @@
-var Heap = function(){
+var Heap = function (keyFn) {
     this._items = [];
     this._keys = [];
     this.size = 0;
+    this.isEmpty = true;
+    this._hash = (keyFn || this._defaultKey);
 };
 
-Heap.prototype.get = function(){
+Heap.fromArray = function (array, keyFn) {
+    var h = new Heap(keyFn);
+    h.push.apply(h, array);
+    return h;
 };
 
-Heap.prototype.put = function(el){
-    this.attach(el);
-    this.size++;
-    this.reorder();
+Heap.prototype.drain = function (other) {
+    while(!other.isEmpty){
+        this.push(other.pop());
+    }
 };
 
-Heap.prototype.delete = function(){
+Heap.prototype.peek = function () {
+    return this._items[0];
 };
 
-Heap.prototype.reorder = function(){
+Heap.prototype.push = function (el) {
+    if (arguments.length > 1){
+        var _this = this;
+        [].slice.call(arguments).forEach(function(a){
+            _this.push(a);
+        });
+    } else {
+        var key = this._hash(el);
+        this._attach(key, el);
+    }
 };
 
-Heap.prototype.attach = function(key, value){
+Heap.prototype.pop = function () {
+    var el = this._items[0];
+    var key = this._keys[0];
+    this.delete(key);
+    return el;
+};
+
+Heap.prototype._heapDown = function (idx) {
+    var childrenIndices = Heap._children(idx);
+    var childKeys = [this._keys[childrenIndices[0]], this._keys[childrenIndices[1]]];
+
+    var thisKey = this._keys[idx];
+    
+    if (!childKeys[0] && childKeys[0]!==0){
+        childKeys[0] = Infinity;
+    }
+    if (!childKeys[1] && childKeys[1]!==0){
+        childKeys[1] = Infinity;
+    }
+
+    if (thisKey > Math.min(childKeys[0],childKeys[1])) {
+        if (childKeys[0] < childKeys[1]){
+            this._swap(childrenIndices[0], idx);
+            this._heapDown(childrenIndices[0]);
+        } else {
+            this._swap(childrenIndices[1], idx);
+            this._heapDown(childrenIndices[1]);
+        }
+    } else {
+        // DONE
+    }
+};
+
+Heap.prototype._heapUp = function (idx) {
+    var parentIndex = Heap._parent(idx);
+    var parentKey = this._keys[parentIndex];
+
+    var thisKey = this._keys[idx];
+
+    if (parentKey > thisKey) {
+        this._swap(parentIndex, idx);
+        this._heapUp(parentIndex);
+    }
+};
+
+Heap.prototype._swap = function (index, index2) {
+    var k = this._keys[index];
+    var v = this._items[index];
+
+    this._keys[index] = this._keys[index2];
+    this._items[index] = this._items[index2];
+
+    this._keys[index2] = k;
+    this._items[index2] = v;
+};
+
+Heap.prototype.delete = function (key) {
+    var idx = this._keys.indexOf(key);
+    this._keys[idx] = null;
+    this._items[idx] = null;
+
+    this._swap(idx, this.size - 1);
+
+    this._heapDown(idx);
+
+    this.size -= 1;
+    this.isEmpty = this.size == 0;
+};
+
+Heap.prototype._defaultKey = function (el) {
+    if (typeof (el) == "number") {
+        return el;
+    }
+    if (typeof (el) == "string") {
+        return el;
+    }
+    if (typeof (el) == "object") {
+        return JSON.stringify(el).toString().length;
+    }
+};
+
+Heap.MIN = function (el) {
+    return Heap.prototype._defaultKey(el);
+};
+
+Heap.MAX = function (el) {
+    return -1 * Heap.prototype._defaultKey(el);
+};
+
+Heap.prototype._attach = function (key, value) {
     this._items[this.size] = value;
     this._keys[this.size] = key;
+
+    this._heapUp(this.size);
+
+    this.size += 1;
+    this.isEmpty = false;
 };
 
+Heap._parent = function (n) {
+    return Math.floor((n - 1) / 2);
+};
+
+Heap._children = function (n) {
+    return [n * 2 + 1, n * 2 + 2];
+};
--- test/Heap/heapTestSpec.js
+++ test/Heap/heapTestSpec.js
@@ -1,52 +1,302 @@
-describe("Heap", function(){
-    
-    it("can be instantiated", function(){
+describe("Heap", function () {
+
+    it("can be instantiated", function () {
         var h = new Heap();
+
+        expect(h).toBeDefined();
+
+        expect(Heap.fromArray).toBeDefined();
+
+        expect(h.drain).toBeDefined();
     });
-    
-    it("has the Heap API", function(){
+
+    it("has the Heap API", function () {
         var h = new Heap();
-        
-        expect(typeof(h.put)).toBe("function");
-        expect(typeof(h.get)).toBe("function");
-        expect(typeof(h.delete)).toBe("function");
+
+        expect(typeof (h.push)).toBe("function");
+        expect(typeof (h.peek)).toBe("function");
+        expect(typeof (h.pop)).toBe("function");
+
     });
-    
-    it("has a size property", function(){
+
+    it("has properties", function () {
         var h = new Heap();
-        
+
         expect(h.size).toBeDefined();
+        expect(h.isEmpty).toBeDefined();
     });
-    
-    it("has internal functions", function(){
+
+    it("has internal functions", function () {
         var h = new Heap();
-        
-        expect(typeof(h.reorder)).toBe("function");
-        expect(typeof(h.attach)).toBe("function");
+
+        expect(typeof (h.delete)).toBe("function");
+        expect(typeof (h._heapUp)).toBe("function");
+        expect(typeof (h._heapDown)).toBe("function");
+        expect(typeof (h._attach)).toBe("function");
     });
 });
 
-describe("Heap put", function(){
-    it("increases size", function(){
+describe("Heap push", function () {
+    it("increases size", function () {
         var h = new Heap();
-        
+
         expect(h.size).toBe(0);
-        
-        h.put(66);
-        
+
+        h.push(66);
+
         expect(h.size).toBe(1);
     });
-    
-    it("triggers reorder", function(){
+
+    it("really increases size", function () {
         var h = new Heap();
+
+        expect(h.size).toBe(0);
+        expect(h.isEmpty).toBe(true);
+
+        h.push(55);
+        h.push(66);
+        h.push(77);
+
+        expect(h.size).toBe(3);
+        expect(h.isEmpty).toBe(false);
+    });
+
+    it("triggers internals", function () {
+        var h = new Heap();
+
+        spyOn(h, "_hash");
+        spyOn(h, "_attach");
+
+        h.push(77);
+
+        expect(h._attach).toHaveBeenCalled();
+        expect(h._hash).toHaveBeenCalled();
+    });
+});
+
+describe("Heap peek", function(){
+    
+    it("gets top element", function(){
+        var h = new Heap();
+        var el = 77;
         
-        spyOn(h, "reorder");
-        spyOn(h, "attach");
+        h.push(el);
         
-        h.put();
+        var retrieved = h.peek();
         
-        expect(h.reorder).toHaveBeenCalled();
-        expect(h.attach).toHaveBeenCalled();
+        expect(retrieved).toBe(el);
     });
     
+    it("gives undef if empty", function(){
+        var h = new Heap();
+        
+        var top = h.peek();
+        
+        expect(top).toBeUndefined();
+    });
+    
+    it("doesn't reduce size", function(){
+        var h = new Heap();
+        
+        h.push(88);
+        
+        expect(h.size).toBe(1);
+        expect(h.isEmpty).toBe(false);
+        
+        h.peek();
+        
+        expect(h.size).toBe(1);
+        expect(h.isEmpty).toBe(false);
+    });
+    
+});
+
+describe("Heap pop", function(){
+    
+    it("gets top element", function(){
+        var h = new Heap();
+        var el = 77;
+        
+        h.push(el);
+        
+        var retrieved = h.pop();
+        
+        expect(retrieved).toBe(el);
+    });
+    
+    it("gives undef if empty", function(){
+        var h = new Heap();
+        
+        var top = h.pop();
+        
+        expect(top).toBeUndefined();
+    });
+    
+    it("reduces size", function(){
+        var h = new Heap();
+        
+        h.push(88);
+        
+        expect(h.size).toBe(1);
+        expect(h.isEmpty).toBe(false);
+        
+        h.pop();
+        
+        expect(h.size).toBe(0);
+        expect(h.isEmpty).toBe(true);
+    });
+    
+    it("really reduces size", function(){
+        var h = new Heap();
+        
+        h.push(88);
+        
+        expect(h.size).toBe(1);
+        expect(h.isEmpty).toBe(false);
+        
+        h.push(88);
+        expect(h.size).toBe(2);
+        
+        h.pop();
+        
+        expect(h.size).toBe(1);
+        expect(h.isEmpty).toBe(false);
+        
+        h.pop();
+        
+        expect(h.size).toBe(0);
+        expect(h.isEmpty).toBe(true);
+    });
+});
+
+describe("Heap behaviour", function(){
+    
+    it("gets minimum key first", function(){
+        var h = new Heap();
+        
+        var min = 55;
+        var med = 66;
+        var max = 77;
+        
+        h.push(med);
+        h.push(min);
+        h.push(max);
+        
+        var retrievedMin = h.pop();
+        var retrievedMed = h.pop();
+        var retrievedMax = h.pop();
+        
+        expect(retrievedMin).toBe(min);
+        expect(retrievedMed).toBe(med);
+        expect(retrievedMax).toBe(max);
+    });
+    
+    it("does work with arbitrary numbers", function(){
+        var h = new Heap();
+        var input = [];
+        for(var i=0;i<100;i++){
+            var r = Math.random();
+            input[i]=r;
+            h.push(r);
+        }
+        
+        var inputSorted = input.slice().sort(function(a,b){
+            return a-b});
+        
+        while(!h.isEmpty){
+            var original = inputSorted.shift();
+            var heapTop = h.pop();
+            expect(heapTop).toBe(original);
+        }
+    });
+    
+    it("can delete random node", function(){
+        var h = new Heap();
+        
+        var min = 55;
+        var med = 66;
+        var max = 77;
+        
+        h.push(med);
+        h.push(min);
+        h.push(max);
+        
+        h.delete(med);
+        
+        var assumedMin = h.pop();
+        var assumedMax = h.pop();
+        
+        expect(assumedMin).toBe(min);
+        expect(assumedMax).toBe(max);
+        expect(h.isEmpty).toBe(true);
+    });
+    
+    it("can merge two trees", function(){
+        var h = new Heap();
+        var k = new Heap();
+        
+        var nums = [1,2,3,4,5,6];
+        
+        for(var i=0;i<6;i++){
+            if (i%2){
+                h.push(nums[i]);
+            } else {
+                k.push(nums[i]);
+            }            
+        }
+        
+        h.drain(k);
+        
+        expect(k.isEmpty).toBe(true);
+        expect(h.size).toBe(6);
+        
+        var retrieved = [];
+        while(!h.isEmpty){
+            retrieved.push(h.pop());
+        }
+        
+        expect(retrieved.toString()).toBe(nums.toString());
+    });
+    
+    it("can do max-heap", function(){
+        var h = new Heap(Heap.MAX);
+        
+        var min = 55;
+        var med = 66;
+        var max = 77;
+        
+        h.push(min,med,max);
+        
+        var assumedMax = h.pop();
+        
+        expect(assumedMax).toBe(max);
+    });
+    
+    it("can do custom keying", function(){
+        var keyer = function(x){ return x.length };
+        var h = new Heap(keyer);
+        
+        var min = "Yamcha";
+        var med = "Son-goku";
+        var max = "Tenshinhan";
+        
+        h.push(med,min,max);
+        
+        var assumedMin = h.pop();
+        var assumedMed = h.pop();
+        var assumedMax = h.pop();
+        
+        expect(assumedMin).toBe(min);
+        expect(assumedMed).toBe(med);
+        expect(assumedMax).toBe(max);
+    });
+    
+    it("supports fromArray", function(){
+        var t = [4,2,545,30292,193,.04];
+        
+        var h = Heap.fromArray(t);
+        
+        expect(h).toBeDefined();
+        expect(h.peek()).toBe(0.04);
+    });
 });
